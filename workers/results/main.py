026a0d11515a003bc52b7b9c8b0425f8abd6e923
OrenGitHub <tuna.is.good.for.you@gmail.com>
2025-08-11 12:13:06 +0300

    trying to debug the CI failure

--- workers/results/main.py
+++ workers/results/main.py
@@ -22,7 +22,7 @@ class Results(AbstractWorker):
     async def run(self, job_id: str) -> None:
         key = self.the_storage_guy.load_results_metadata_from_db(job_id)
         content = await self.the_storage_guy.load_results(key)
-        sarif_results = {}
+        sarif_results = {'debug': 'query engine failed'}
         if ': yes' in content:
             p = Results.parse_proper_path(content)
             sarif_results = Results.generate_sarif_from_path(p)
